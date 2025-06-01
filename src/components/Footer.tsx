5f521c1ffba55049a3e54a9802733d0ca46cc6ba
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2025-06-01 13:44:28 -0700

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -64,9 +64,9 @@ export const Footer: React.FC = () => {
               href="https://github.com"
               target="_blank"
               rel="noopener noreferrer"
-              className=" hover:text-accent transition-colors"
+              className="text-hookersgreen hover:text-accent transition-colors"
             >
-              <Github className="h-4 w-4 text-hookersgreen" />
+              <Github className="h-4 w-4" />
             </a>
             <a
               href="https://linkedin.com"
