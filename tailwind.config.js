7775deba14514d446531bf818db31291686e740a
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2025-06-01 13:40:24 -0700

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: '#0B3B3C',
-        secondary: '#3d5c56',
-        accent: '#EFC88B',
-        neutral: '#FFFFFF',
-        muted: '#F3F1EE',
-        thistle: '#BFACC8',
+        primary: "#0B3B3C",
+        secondary: "#3d5c56",
+        accent: "#EFC88B",
+        neutral: "#FFFFFF",
+        muted: "#F3F1EE",
+        thistle: "#BFACC8",
+        hookersgreen: "#6d8a83",
       },
       fontFamily: {
-        sans: ['"Open Sans"', 'sans-serif']
+        sans: ['"Open Sans"', "sans-serif"],
       },
       animation: {
-        'fade-in': 'fadeIn 0.5s ease-in-out',
-        'slide-up': 'slideUp 0.5s ease-out',
-        'scale': 'scale 0.2s ease-in-out',
+        "fade-in": "fadeIn 0.5s ease-in-out",
+        "slide-up": "slideUp 0.5s ease-out",
+        scale: "scale 0.2s ease-in-out",
       },
       keyframes: {
         fadeIn: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
         },
         slideUp: {
-          '0%': { transform: 'translateY(20px)', opacity: '0' },
-          '100%': { transform: 'translateY(0)', opacity: '1' },
+          "0%": { transform: "translateY(20px)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
         },
         scale: {
-          '0%': { transform: 'scale(1)' },
-          '50%': { transform: 'scale(0.95)' },
-          '100%': { transform: 'scale(1)' },
+          "0%": { transform: "scale(1)" },
+          "50%": { transform: "scale(0.95)" },
+          "100%": { transform: "scale(1)" },
         },
       },
     },
   },
   plugins: [],
-}
+};
