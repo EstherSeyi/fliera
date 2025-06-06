f61c179439a042a06bac2f87e1ef5025449849ed
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2025-06-06 11:26:54 +0100

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -75,7 +75,7 @@ export const Navbar: React.FC = () => {
                   whileHover={{ scale: 1.05 }}
                   whileTap={{ scale: 0.95 }}
                   onClick={logout}
-                  className="flex items-center px-6 py-2 bg-primary/10 text-primary hover:bg-primary/20 transition-colors"
+                   className="flex items-center px-6 py-2 bg-primary/10 text-primary hover:bg-primary/20 transition-colors rounded-lg"
                 >
                   <LogOut className="w-5 h-5 mr-2" />
                   Logout
