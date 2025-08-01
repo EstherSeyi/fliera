b5471e90cf34a4e60a0ae16c5e6067f993b7d5f0
Esther Ogundijo <sheyiogundijo@gmail.com>
2025-08-01 22:43:53 +0100

    adds query key for get public events

--- src/hooks/queries/constants.ts
+++ src/hooks/queries/constants.ts
@@ -1,3 +1,4 @@
 export const GET_DASHBOARD_STATS_KEY = "get-dashboard-stats";
 export const GET_RECENT_EVENTS_KEY = "get-recent-events";
 export const GET_EVENT_DETAILS_KEY = "get-event-details";
+export const GET_PUBLIC_EVENTS_KEY = "get-public-events";
